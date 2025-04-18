cce4459ad7b648718c31306e560e80c375926409
nightfury214 <207950995+nightfury214@users.noreply.github.com>
2025-04-17 18:41:35 -0700

    fix:: add permission

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Snippet
-# Register your models here.
+
 @admin.register(Snippet)
 class SnippetAdmin(admin.ModelAdmin):
     readonly_fields = ['highlighted']
--- snippets/models.py
+++ snippets/models.py
@@ -3,7 +3,6 @@ from pygments import highlight
 from pygments.formatters.html import HtmlFormatter
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
-# Create your models here.
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
